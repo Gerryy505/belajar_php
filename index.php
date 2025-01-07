11980d800d9bf5e46c62f4e343602bd176ed6e5e
Gerryy505 <syahrezagerry4@gmail.com>
2025-01-07 12:10:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H3>
+      <?php
+         echo "Hello world!!! Ini adalah halaman pertama php";
+
+        ?>
+    </H3>
+</body>
+</html>
